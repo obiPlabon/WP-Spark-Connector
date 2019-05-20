5053d1f211930d1641e596e53844c80bc6b6095a
Md. Anam Hossain <anam@themexpert.com>
2019-05-20 11:28:11 +0600

    plugin information updated

--- spark-connector.php
+++ spark-connector.php
@@ -1,10 +1,10 @@
 <?php 
 /**
- * Plugin Name: TG App Connector
- * Plugin URI: https://themesgrove.com
- * Author: Themesgrove
- * Author URI: https://themesgrove.com
- * Description: A Connector Plugin for Themesgrove Applications
+ * Plugin Name: Spark App Connector
+ * Plugin URI: https://wpspark.io/
+ * Author: WP Spark
+ * Author URI: https://wpspark.io/
+ * Description: A Connector Plugin for WP Spark Applications
  * Version:1.0
  * License: GPLv2 or Later
  *  */ 
@@ -19,20 +19,20 @@ if ( ! defined( 'WPINC' ) ) {
 /**
  * define the core root file
  */
-define( 'TGC_CORE_ROOT', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'SPARK_CORE_ROOT', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 /**
 * plugin activation library
 */
-require TGC_CORE_ROOT. '/libs/class-tgm-plugin-activation.php';
+require SPARK_CORE_ROOT. '/libs/class-tgm-plugin-activation.php';
 
 /**
  * require files here
  */
-require TGC_CORE_ROOT. '/inc/plugin_activation.php';
-require TGC_CORE_ROOT. '/inc/connector.php';
-require TGC_CORE_ROOT. '/inc/admin_menu.php';
-require TGC_CORE_ROOT. '/inc/routes.php';
+require SPARK_CORE_ROOT. '/inc/plugin_activation.php';
+require SPARK_CORE_ROOT. '/inc/connector.php';
+require SPARK_CORE_ROOT. '/inc/admin_menu.php';
+require SPARK_CORE_ROOT. '/inc/routes.php';
 
 function spark_core_load(){
 	TGC_Admin_Menu::init();
@@ -45,17 +45,17 @@ add_action('plugins_loaded', 'spark_core_load');
  * Flush rewrite rules on 
  * plugin activation/deactivation
  */
-function tgc_core_flush() {
+function spark_core_flush() {
     flush_rewrite_rules();
 }
-register_activation_hook( __FILE__, 'tgc_core_flush' );
-register_deactivation_hook( __FILE__, 'tgc_core_flush' );
+register_activation_hook( __FILE__, 'spark_core_flush' );
+register_deactivation_hook( __FILE__, 'spark_core_flush' );
 
 /**
  * Load script to admin pages
  */
-add_action('admin_enqueue_scripts', 'tgc_load_script_to_admin');
-function tgc_load_script_to_admin(){
+add_action('admin_enqueue_scripts', 'spark_load_script_to_admin');
+function spark_load_script_to_admin(){
 	wp_enqueue_style('tgc-core', plugin_dir_url(__FILE__). 'assets/css/style.css');
 
 	wp_enqueue_script('new-script', plugin_dir_url(__FILE__). 'assets/js/newScript.js', array('jquery'), '1.0', false);
