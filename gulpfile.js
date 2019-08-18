7dca0e3df82916075ffa5fc641c6814b761c99bf
anamwp <anam@themexpert.com>
2019-08-18 14:58:54 +0600

    gulp package command added

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+gulp.task('package', async function () {
+  gulp.src([
+      './**',
+      './*/**',
+      '!./bower_components/**',
+      '!./node_modules/**',
+      '!./bower_components',
+      '!./node_modules',
+      '!gulpfile.js',
+      '!package.json',
+      '!./assets',
+      '!*.json',
+      '!*.config.js',
+      '!*.lock',
+      '!*.phar',
+      '!*.xml',
+  ])
+    .pipe(zip('spark.zip'))
+    .pipe(gulp.dest('.'));
+});
